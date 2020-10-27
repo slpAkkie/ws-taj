5db20af35a7c2d56cf84ba640789d33942af91ab
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2020-10-27 18:00:52 +0300

    Changed paths Changed paths to load resources without using a server

--- src/js/modules/game-data.js
+++ src/js/modules/game-data.js
@@ -1,18 +1,18 @@
 const resourcePath = {
   'Character': {
     'Jerry': {
-      'stay': '../img/sprites/jerry-stay.png',
-      'walk': '../img/sprites/jerry-walk.png',
+      'stay': 'img/sprites/jerry-stay.png',
+      'walk': 'img/sprites/jerry-walk.png',
     },
     'Tuffy': {
-      'stay': '../img/sprites/tuffy-stay.png',
-      'walk': '../img/sprites/tuffy-walk.png',
+      'stay': 'img/sprites/tuffy-stay.png',
+      'walk': 'img/sprites/tuffy-walk.png',
     },
   },
-  'Enemy': '../img/sprites/tom-walk.png',
-  'Hill': '../img/Chair.png',
-  'Cheese': '../img/Cheese.png',
-  'BG': '../img/BG.jpg',
+  'Enemy': 'img/sprites/tom-walk.png',
+  'Hill': 'img/Chair.png',
+  'Cheese': 'img/Cheese.png',
+  'BG': 'img/BG.jpg',
 };
 
 let playerInfo = {
