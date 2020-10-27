5d51bdda3b3f249437ff02a9d9351434ff4689ce
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2020-10-27 17:42:01 +0300

    Added new includes - event-handler - game-data - Game.class - init

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+// Сторонние библиотеки
+//= ../../node_modules/jquery/dist/jquery.min.js
+
+// Скрипты игры
+//= modules/game-data.js
+//= modules/init.js
+//= modules/Game.class.js
+//= modules/event-handler.js
