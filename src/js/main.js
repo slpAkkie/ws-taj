6030a43d45d60ebc374bf8bb4db9f72a59035495
slpakkie <sha.alexandr.se@yandex.ru>
2020-10-31 22:55:49 +0300

    Added includes

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,21 @@
 //= ../../node_modules/jquery/dist/jquery.min.js
 
 // Скрипты игры
+//= modules/resourceClasses/BackgroundResource.class.js
+//= modules/resourceClasses/CharacterResource.class.js
+//= modules/resourceClasses/CollectedResource.class.js
+//= modules/resourceClasses/EnemyResource.class.js
+//= modules/resourceClasses/HillResource.class.js
 //= gameResources.js
 //= gameData.js
+//= modules/Point.class.js
+//= modules/EntityImage.class.js
+//= modules/EntitySprite.class.js
+//= modules/SimpleObject.class.js
+//= modules/Entity.class.js
+//= modules/Enemy.class.js
+//= modules/Player.class.js
+//= modules/UI.class.js
+//= modules/Background.class.js
 //= modules/Game.class.js
 //= eventHandler.js
