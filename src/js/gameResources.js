5f886381f42a3266675829d649f1c21391312e7c
slpakkie <sha.alexandr.se@yandex.ru>
2020-10-31 22:54:29 +0300

    Rename resourceData to gameResources
    Now resources will be in window.gameResources
    And they are already in object wrap

--- src/js/gameResources.js
+++ src/js/gameResources.js
@@ -3,5 +3,50 @@
  */
 
 window.gameResources = {
-
+  'jerry:idle': new EntityImage( {
+    'path': '..\\img\\sprites\\jerry-idle.png',
+    'width': 28,
+    'height': 42,
+  } ),
+  'jerry:walk': new EntitySprite( {
+    'path': '..\\img\\sprites\\jerry-walk.png',
+    'frameCount': 8,
+    'width': 28,
+    'height': 42,
+    'animationDuration': 1000,
+  } ),
+  'tuffy:idle': new EntityImage( {
+    'path': '..\\img\\sprites\\tuffy-idle.png',
+    'width': 28,
+    'height': 27,
+  } ),
+  'tuffy:walk': new EntitySprite( {
+    'path': '..\\img\\sprites\\tuffy-walk.png',
+    'frameCount': 3,
+    'width': 28,
+    'height': 27,
+    'animationDuration': 300,
+  } ),
+  'tom:walk': new EntitySprite( {
+    'path': '..\\img\\sprites\\tom-walk.png',
+    'frameCount': 15,
+    'width': 32,
+    'height': 45,
+    'animationDuration': 1900,
+  } ),
+  'hill': new EntityImage( {
+    'path': '..\\img\\Chair.png',
+    'width': 1500,
+    'height': 1500,
+  } ),
+  'cheese': new EntityImage( {
+    'path': '..\\img\\Cheese.png',
+    'width': 137,
+    'height': 95,
+  } ),
+  'bg': new EntityImage( {
+    'path': '..\\img\\BG.png',
+    'width': 1599,
+    'height': 837,
+  } ),
 }
