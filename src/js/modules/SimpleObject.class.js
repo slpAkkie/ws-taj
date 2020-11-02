4d9d71d7a3abecab14e873e4f3d84cd3cdef07f4
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2020-11-02 21:55:11 +0300

    Finished

--- src/js/modules/SimpleObject.class.js
+++ src/js/modules/SimpleObject.class.js
@@ -1,3 +1,9 @@
+/** Простой класс игрового объекта, имеющий координаты и картинку */
 class SimpleObject {
+
+  constructor( resourceKey ) {
+    this.coords = new Coords( 0, 0 );
+    this.img = gameResources[ resourceKey ];
+  }
 
 }
