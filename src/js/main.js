949a9bb4ac0abd2875751aa2fb3c05f803e0fd1d
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2020-11-02 21:52:37 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -2,17 +2,14 @@
 //= ../../node_modules/jquery/dist/jquery.min.js
 
 // Скрипты игры
-//= modules/resourceClasses/BackgroundResource.class.js
-//= modules/resourceClasses/CharacterResource.class.js
-//= modules/resourceClasses/CollectedResource.class.js
-//= modules/resourceClasses/EnemyResource.class.js
-//= modules/resourceClasses/HillResource.class.js
+//= modules/resourceClasses/EntityImage.class.js
+//= modules/resourceClasses/EntitySprite.class.js
 //= gameResources.js
 //= gameData.js
-//= modules/Point.class.js
-//= modules/EntityImage.class.js
-//= modules/EntitySprite.class.js
+//= modules/Coords.class.js
 //= modules/SimpleObject.class.js
+//= modules/CollectableItem.class.js
+//= modules/Hill.class.js
 //= modules/Entity.class.js
 //= modules/Enemy.class.js
 //= modules/Player.class.js
