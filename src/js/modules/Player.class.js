d44ba1dad46a01de3a9e73d6ec5ec9de64ece222
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2020-11-02 21:53:57 +0300

    First step

--- src/js/modules/Player.class.js
+++ src/js/modules/Player.class.js
@@ -1,3 +1,17 @@
 class Player extends Entity {
 
+  hp = 100;
+  eatenCheese = 0;
+  speed = 500;
+  direction = 1;
+  currentState = 'idle';
+
+  set hp( v ) {
+    this.hp = ( v > 100 ? 100 : v < 0 ? 0 : v );
+  }
+
+  get width() {
+    return this.state[ this.currentState ].width;
+  }
+
 }
